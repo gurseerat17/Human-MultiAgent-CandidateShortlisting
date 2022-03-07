487d1e4a12dcc48043c6f6a2e045f5d7daeb47e2
nk-ag <niki.darj@gmail.com>
2022-03-07 11:01:15 +0530

    functioning version

--- preprocessing/tf_idf_lemmetizer.py
+++ preprocessing/tf_idf_lemmetizer.py
@@ -1,7 +1,7 @@
 from nltk.stem.wordnet import WordNetLemmatizer
 from sklearn.feature_extraction.text import TfidfVectorizer
 from nltk.corpus import stopwords as stp
-from preprocessing import lemma_tagger as tag
+from ..preprocessing import lemma_tagger as tag
 
 lemmatizer = WordNetLemmatizer()
 analyzer = TfidfVectorizer().build_analyzer()
